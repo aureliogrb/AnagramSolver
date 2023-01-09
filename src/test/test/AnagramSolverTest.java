8920c90112a162fa242eb23f84afc075e455faa0
Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>
2023-01-09 10:34:49 -0800

    update
    Signed-off-by: Aurelio Garcia-Ribeyro <aurelio.garciaribeyro@oracle.com>

---
+++ src/test/test/AnagramSolverTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+public class AnagramSolverTest {
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
